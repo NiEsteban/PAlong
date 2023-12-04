d2b6e56e408284ab560f17f27d3c5aec2fb97fd4
NiEsteban <132994623+NiEsteban@users.noreply.github.com>
2023-12-04 17:16:24 +0100

    Update loop.py

--- loop.py
+++ loop.py
@@ -4,10 +4,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 speed_mode = 0 #selected : 0, managed = 1
 alt_mode = "Managed" #Selected ou FPA ou VS
+k11 = 0.08
+k22 = 2
+nz_max = 10
+nz_min = 0
 nz=1
 nx=1
-lim_nx=
-lim_nz=
+lim_nx=0
+lim_nz=0
+odl_state = {"x":0,"y":0,"z":0,"IAS":0,"fpa":0,"phi":0,"psi":0}
 state_vector = {"x":0,"y":0,"z":0,"IAS":0,"fpa":0,"phi":0,"psi":0}
 def op(agent, connected):
     pass
@@ -16,7 +21,12 @@ def op(agent, connected):
 def cl(agent,_id):
     IvyStop()
 
-
+def apply_lim(value, max, min=0):
+    if value>max:
+        return max
+    elif value< min:
+        return min
+    else value
 
 def msg(agent, *larg):
     print(larg)
@@ -31,20 +41,19 @@ def capture_pente(agent, *larg):
 
 def capture_alt(agent, *larg):
     alt_mode = larg[2]
-    zc = larg[1]
-    if alt_mode == 0 :
-        t = np.linspace(0,500,501)
-        dt=1/500
-        state_vector["fpa"] = np.arcsin(zc-state_vector["z"]/state_vector["IAS"]*dt)
+    if alt_mode == "Selected":
+        zc = larg[1]
+        nz = ((zc-z)/v*k11-state_vector["fpa"])*k22+np.cos(state_vector["fpa"])/np.cos(state_vector["phi"])
+        return apply_lim(nz,nz_max,nz_min)
+    elif alt_mode == "Managed":
 
 
-        return (nz,nx)
-    else:
+    elif alt_mode == "FPA":
 
 
 
 
-        return (nz,nx)
+        return apply_lim(nz,nz_max,nz_min)
 
 
 app_n ="Testap"
